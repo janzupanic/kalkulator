5dc247dc348f34aa380aa6334188baf7a16cbdd6
Jan <150776762+janzupanic@users.noreply.github.com>
2024-10-27 16:53:35 +0100

    Zamjena App.jsx
    zamjena kalkulators.jsx i app.jsx

--- kalkulator-main/src/App.jsx
+++ kalkulator-main/src/App.jsx
@@ -1,48 +1,15 @@
-import { createSignal } from 'solid-js';
+import styles from './App.module.css';
 import Kalkulator from './kalkulator';
 
-const App = () => {
-  const [ciljaniIznos, setCiljaniIznos] = createSignal('');
-  const [mjesecnaStednja, setMjesecnaStednja] = createSignal('');
-  const [rezultat, setRezultat] = createSignal(null);
-
-  // Funkcija koja će primiti rezultat iz Kalkulator komponente
-  const handleIzracunaj = (mjeseci) => {
-    setRezultat(mjeseci);
-  };
-
+function App() {
   return (
-    <div>
-      <h1>Kalkulator Vremena Štednje</h1>
-      <div>
-        <label>Ciljani iznos: </label>
-        <input
-          type="number"
-          value={ciljaniIznos()}
-          onInput={(e) => setCiljaniIznos(e.target.value)}
-          placeholder="Unesite ciljani iznos"
-        />
-      </div>
-      <div>
-        <label>Mjesečna štednja: </label>
-        <input
-          type="number"
-          value={mjesecnaStednja()}
-          onInput={(e) => setMjesecnaStednja(e.target.value)}
-          placeholder="Unesite mjesečnu štednju"
-        />
-      </div>
-      <Kalkulator
-        ciljaniIznos={ciljaniIznos()}
-        mjesecnaStednja={mjesecnaStednja()}
-        onIzracunaj={handleIzracunaj}
-      />
-      {rezultat() && (
-        <div>
-          <h2>Za uštedu iznosa bit će potrebno: {rezultat()} mjeseci</h2>
-        </div>
-      )}
+    <>
+    <div class={styles.App}>
+      <h1>Kalkulator Vremena Štednje </h1>
+      <Kalkulator/>
     </div>
+    </>
+
   );
 };
 
